99f0125468c02d7b3080cc7d05705dc1404b85f9
Paola Santillan <a.paolasantillangzz@hotmail.com>
2022-10-05 01:16:18 -0500

    Avance proyecto - EDespliegue de Información del producto,structura de componentes y diseño

--- src/components/ComponenteArticle.jsx
+++ src/components/ComponenteArticle.jsx
@@ -1,31 +1,30 @@
 // INICIO
 
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { viewArticles } from "../funciones/funciones";
-
 
 const Article = () => {
     const [articles, setArticles] = useState(null)
 
-    useEffect(()=>{
+    useEffect(() => {
         viewArticles(setArticles)
-    },[])
-     
+    }, [])
+
     return (
-        <>
-        {articles == null ? ("no hay articulos") : 
-        articles.map(articulo => (
-            <div key={articulo._id}>
-                <a href={`/articulo/${articulo._id}`}>{articulo.brand}</a>
-                <img src={articulo.image} alt="#" />
-                <h3>{articulo.product_name}</h3>
-                <h4>{articulo.description}</h4>
-                <p>${articulo.price}</p>
-            </div>
-        ))}
-        
-        </>
+        <div className="row">
+            {articles == null ? ("no hay articulos") :
+                articles.map(articulo => (
+                    <div key={articulo._id} className="col">
+                        <div className="card">
+                        <img src={articulo.image} alt="" />
+                        <h3>{articulo.product_name}</h3>
+                        <p>${articulo.price}</p>
+                        <a className="button" href={`/articulo/${articulo._id}`}>Mostrar producto</a>
+                        </div>
+                    </div>
+                ))}
+        </div>
     )
 }
 
