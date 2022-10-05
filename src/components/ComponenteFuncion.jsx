42915781fcd7c9b914a0c6819e0d0c4d47b0c8af
Paola Santillan <a.paolasantillangzz@hotmail.com>
2022-10-05 16:05:42 -0500

    design product section

--- src/components/ComponenteFuncion.jsx
+++ src/components/ComponenteFuncion.jsx
@@ -1,6 +1,6 @@
 // PERSONAJE
 
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { uniqueArticle } from "../funciones/funciones";
 
@@ -9,19 +9,44 @@ const Inicio = () => {
     const params = useParams()
     useEffect(() => {
         uniqueArticle(params._id, setArticles)
-    },[])
+    }, [])
 
     return (
-        <>
-        {articulo == null ? ("no hay articulos") : 
-        (
-            <div>
-                <h2>Articulo con el id {params._id}</h2>
-                <p>Con el nombre {articulo.brand}</p>
-                <img src={articulo.image} alt="" />
-            </div>
-        )}
-        </>
+        <body className="body">
+            <>
+                {articulo == null ? ("no hay articulos") :
+                    (
+                        <div>
+                            <div className="articlesect">
+                                <div>
+                                    <h1 className="title-product">{articulo.product_name}</h1>
+                                    <img className="img-product" src={articulo.image} alt="" />
+                                </div>
+                                <div className="section-articles">
+                                    <p className="desc-product">Descripción: {articulo.description}</p>
+                                    <div className="star-section">
+                                        <i class="star fa-solid fa-star"></i>
+                                        <i class="star fa-solid fa-star"></i>
+                                        <i class="star fa-solid fa-star"></i>
+                                        <i class="star fa-solid fa-star"></i>
+                                        <i class="star fa-solid fa-star"></i>
+                                    </div>
+                                    <hr />
+
+                                    <button className="btn-car"><i class="fa-solid fa-cart-arrow-down"></i> Agregar al carrito</button>
+                                    <div className="sect-datos">
+                                        <h4 className="brand-product">Precio: ${articulo.price}</h4>
+                                        <h2 className="brand-product">Marca: {articulo.brand}</h2>
+                                        <h4 className="brand-product">Categoría: {articulo.category}</h4>
+                                        <button className="brand-product btn-buy"><i class="fa-solid fa-truck"></i> Comprar</button>
+                                        <p className="date-product">La oferta vence el: {articulo.updatedAt}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+            </>
+        </body>
     )
 }
 
