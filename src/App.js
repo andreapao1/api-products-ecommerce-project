2de4ef5363a6d5de46b1080332e473047dbf2639
Paola Santillan <a.paolasantillangzz@hotmail.com>
2022-10-11 22:09:49 -0500

    setting to upload project to github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import React, {useState} from 'react';
 /* eslint-disable react/react-in-jsx-scope */
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter , Routes, Route } from "react-router-dom";
 import Inicio from "./components/ComponenteArticle";
 import Article from "./components/ComponenteFuncion";
 // eslint-disable-next-line no-unused-vars
@@ -14,12 +14,12 @@ function App() {
 	return (
 		<div className="contenedor">
 			{/* <NavComponent searchFunction={"searchFunction"}/> */}
-			<BrowserRouter>
+			<HashRouter >
 				<Routes>
 					<Route path='/' element={<Inicio></Inicio>}></Route>
 					<Route path='/articulo/:_id' element={<Article></Article>}></Route>
 				</Routes>
-			</BrowserRouter>
+			</HashRouter >
 		</div> 
 	); 
 }
